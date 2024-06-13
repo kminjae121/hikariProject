ce2e1dd42dc55ec592ecd3d96f42a35fc5238de4
Nutubit17 <koobonhoo@gmail.com>
2024-06-13 22:47:48 +0900

    feat : Tutorial Object

--- Assets/01_MemberFile/Kbh/01Scripts/Logic/TutorialObject.cs
+++ Assets/01_MemberFile/Kbh/01Scripts/Logic/TutorialObject.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -15,104 +16,240 @@ public class TutorialObject : MonoBehaviour
       Click = 2
    }
 
+   [System.Serializable]
    public struct TutorialContentStruct
    {
+      [TextArea(2,3)] public string text;
       public TutorialEventEnum nextOverType;
-      public UIBowl[] contents;
-      public string text;
+      public CanvasGroup contents;
+      public RectTransform btnGoalTrm;
       public float delay;
    }
 
+   public event Action OnEndEvent = null;
+   [SerializeField] private bool _isEnable = false;
+   [SerializeField] private bool _isChecking = false;
+   [SerializeField] private bool _isTransition = false;
+
    [SerializeField] private TextMeshProUGUI _textDisplay;
    [SerializeField] private Button _selectGuideButton;
+   private Vector3 _selectGuideStartPosition;
 
    [SerializeField] private TutorialContentStruct[] _tutorialContentList;
+   
 
-   private bool _isEnable = false;
+   [Header("Additional Setting")]
+   [SerializeField] private bool _isWithFade = true;
+   [SerializeField] private float _fadeTime = 0.5f;
+   [Space(10)]
+   [SerializeField] private bool _isWithBtnInterpolation = true;
+   [SerializeField] private float _btnInterpolationTime = 0.5f;
+   [SerializeField] private Ease _btnInterpolationEase = Ease.Linear;
+
+
    private int _currentContentIdx = 0;
    private TutorialContentStruct _previousContent;
    private TutorialContentStruct _currentContent;
-   private bool _isWait = true;
    private float _lastTimeStamp;
 
-   private void OnEnable()
+
+   private void Awake()
+   {
+      _selectGuideStartPosition
+         = _selectGuideButton.transform.position;
+      Initialize();
+   }
+
+   [ContextMenu("Initialize")]
+   private void Initialize()
    {
       _selectGuideButton.onClick.AddListener(HandleSelectGuideButtonClick);
+      
+      _currentContentIdx = 0;
+
       _previousContent = new TutorialContentStruct();
       _currentContent = _tutorialContentList[0];
-      _isEnable = true;
+
+      EnableAction();
+   }
+
+   private void EnableAction()
+   {
+      _selectGuideButton.gameObject.SetActive(true);
+
+      if (_isWithFade)
+      {
+         _currentContent.contents.DOFade(1, _fadeTime);
+         
+         DOVirtual.DelayedCall(_fadeTime, () =>
+         {
+            _isChecking = true;
+            _isEnable = true;
+            _lastTimeStamp = Time.time;
+            _currentContent.contents.interactable = true;
+            _currentContent.contents.blocksRaycasts = true;
+         });
+      }
+      else
+      {
+
+         _currentContent.contents.alpha = 1;
+
+         _isChecking = true;
+         _isEnable = true;
+         _lastTimeStamp = Time.time;
+         _currentContent.contents.interactable = true;
+         _currentContent.contents.blocksRaycasts = true;
+      }
+
+      _selectGuideButton.image.color = Color.white;
+      _selectGuideButton.transform.position
+         = _selectGuideStartPosition;
+
+      _textDisplay.text = _currentContent.text;
    }
 
    private void HandleSelectGuideButtonClick()
    {
       if ((int)(_currentContent.nextOverType & TutorialEventEnum.Click) > 0)
       {
-         _isWait = false;
+         OnCheckSuccess();
       }
+   }
+
+   private void OnCheckSuccess()
+   {
+      _isChecking = false;
+      _lastTimeStamp = Time.time;
    }
 
    private void Update()
    {
       if (!_isEnable) return;
 
-      if (_isWait)
+      if (_isChecking)
       {
-         CheckFlipNextContent();
+         CheckCondition();
       }
       else
       {
-         FlipNextContent();
-      }
-
-
-   }
-
-   private void FlipNextContent()
-   {
-      if(_previousContent.nextOverType != TutorialEventEnum.Null)
-      {
-         foreach(var content in _previousContent.contents)
-         {
-         }
+         if (_isTransition) return;
+         TransitionToNextContent();
       }
    }
 
-   private void CheckFlipNextContent()
+   private void CheckCondition()
    {
       if ((_currentContent.nextOverType & TutorialEventEnum.Time) == 0) return;
       if (_lastTimeStamp + _currentContent.delay < Time.time)
       {
-         _isWait = false;
-         _lastTimeStamp = Time.time;
-         _previousContent = _currentContent;
-
-         _currentContentIdx++;
-
-         while (_tutorialContentList[_currentContentIdx].nextOverType == TutorialEventEnum.Null
-            && _currentContentIdx == _tutorialContentList.Length)
-         {
-            ++_currentContentIdx;
-         }
-
-         if (_currentContentIdx == _tutorialContentList.Length)
-         {
-            _isEnable = false;
-            DisableAction();
-            return;
-         }
-
-         _currentContent = _tutorialContentList[_currentContentIdx];
+         OnCheckSuccess();
       }
    }
 
-   private void DisableAction()
+   private void TransitionToNextContent()
    {
+
+      while (_currentContentIdx + 1 != _tutorialContentList.Length
+         && _tutorialContentList[_currentContentIdx + 1].nextOverType == TutorialEventEnum.Null)
+      {
+         ++_currentContentIdx;
+      }
+
+      if (_currentContentIdx + 1 == _tutorialContentList.Length)
+      {
+         _isEnable = false;
+         DisableAction();
+         return;
+      }
+
+      _previousContent = _currentContent;
+      _currentContent = _tutorialContentList[++_currentContentIdx];
+
+      UpdateContent();
+
+      
+      if (_isWithFade)
+      {
+         _isTransition = true;
+         DOVirtual.DelayedCall(_fadeTime, () =>
+         {
+            _isChecking = true;
+            _isTransition = false;
+         });
+      }
+      else
+      {
+         _isChecking = true;
+      }
    }
 
-   private void OnDisable()
+
+   private void UpdateContent()
    {
-      
+      if (_previousContent.nextOverType != TutorialEventEnum.Null)
+      {
+         if (_isWithFade)
+         {
+            _previousContent.contents.DOFade(0, _fadeTime);
+            _currentContent.contents.DOFade(1, _fadeTime);
+         }
+         else
+         {
+            _previousContent.contents.alpha = 0;
+            _currentContent.contents.alpha = 1;
+         }
+
+         bool isExistsNextGoalPosition
+            = _currentContent.btnGoalTrm;
+         if (_isWithBtnInterpolation && isExistsNextGoalPosition)
+         {
+            _selectGuideButton.interactable = false;
+
+            _selectGuideButton.transform
+               .DOMove(_currentContent.btnGoalTrm.position,
+                   _btnInterpolationTime).SetEase(_btnInterpolationEase)
+               .OnComplete(() => _selectGuideButton.interactable = true);
+         }
+         else
+         {
+            _selectGuideButton.transform.position
+               = _currentContent.btnGoalTrm.position;
+         }
+
+
+         _previousContent.contents.interactable = false;
+         _previousContent.contents.blocksRaycasts = false;
+
+         _currentContent.contents.interactable = true;
+         _currentContent.contents.blocksRaycasts = true;
+
+         _textDisplay.text = _currentContent.text;
+      }
    }
+
+
+   private void DisableAction()
+   {
+      OnEndEvent?.Invoke();
+      _selectGuideButton.onClick.RemoveListener(HandleSelectGuideButtonClick);
+      _selectGuideButton.gameObject.SetActive(false);
+
+      if (_isWithFade)
+      {
+         _tutorialContentList[_tutorialContentList.Length - 1]
+            .contents.DOFade(0, _fadeTime);
+         _textDisplay.text = "";
+      }
+      else
+      {
+         _tutorialContentList[_tutorialContentList.Length - 1]
+            .contents.alpha = 0;
+         _textDisplay.text = "";
+      }
+   }
+
+   
 
 
 }
