3893a8a5301046176b03f5289bef2ee98e0f919b
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2024-06-19 18:40:56 +0900

    audiomixerSlide

--- Assets/01_MemberFile/KMJ/Script/SoundSlider.cs
+++ Assets/01_MemberFile/KMJ/Script/SoundSlider.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundSlider : MonoBehaviour
 {
-
     public AudioMixer audioMixer;
+    public float maxVolume = 20;
+    public float minVolume = -80;
+
+    private void Awake()
+    {
+        SetMusicVolume(0);
+    }
+
     public void SetMusicVolume(float volume)
     {
-        audioMixer.SetFloat("BackGround", Mathf.Log10(volume));
+        audioMixer.SetFloat("BackGround", volume * maxVolume);
     }
 }
