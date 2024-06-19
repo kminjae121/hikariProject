d7201b23660889409e83d1d8f3bbb9727b70ee88
rlaals0203 <scientistmin@gmail.com>
2024-06-19 18:24:42 +0900

    remove : KimMin's GameManager

--- Assets/01_MemberFile/Kbh/01Scripts/Core/GameManager_Kbh.cs
+++ Assets/01_MemberFile/Kbh/01Scripts/Core/GameManager_Kbh.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager_Kbh : MonoSingleton<GameManager>
+public class GameManager_Kbh : MonoSingleton    <GameManager>
 {
    Player_Kbh player; 
 
