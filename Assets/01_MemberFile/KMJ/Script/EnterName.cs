44a6af5f292ea979e690bab3c23515b0cf58d1a2
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2024-06-20 18:30:14 +0900

    Fixed NameEnter

--- Assets/01_MemberFile/KMJ/Script/EnterName.cs
+++ Assets/01_MemberFile/KMJ/Script/EnterName.cs
@@ -16,9 +16,17 @@ public class EnterName : MonoBehaviour
 
     public void Enter()
     {
-        receiveText.text = $"사용자이름: {display.text}";
+        if(display.text.Length < 5)
+        {
+            receiveText.text = $"사용자이름: {display.text}";
+        }
+        else
+        {
+            receiveText.text = $"다시 입력하세요";
+        }
         PlayerPrefs.SetString("이름 입력", receiveText.text);
         PlayerPrefs.Save();
+        
     }
 
 
