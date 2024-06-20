9407b0455da4261a9ea7303ea96d096cd9142ab6
fishmaster <a01022639410@gmail.com>
2024-06-20 19:29:54 +0900

    Desigh : Esc Menu

--- Assets/01_MemberFile/KYJ/01. Scripts/PlayerMovement.cs
+++ Assets/01_MemberFile/KYJ/01. Scripts/PlayerMovement.cs
@@ -5,43 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private PriviewWindow priviewWindow;
+    //private PriviewWindow priviewWindow;
     private Rigidbody2D rigid;
 
     private bool isCoolTime;
-    [SerializeField] private float speed = 5;
+    [SerializeField] private float speed;
 
 
     private void Awake()
     {
-        priviewWindow = GameObject.Find("PriviewWindow").GetComponent<PriviewWindow>();
+        //priviewWindow = GameObject.Find("PriviewWindow").GetComponent<PriviewWindow>();
         rigid = GetComponent<Rigidbody2D>();
     }
 
 
     private void Update()
     {
-        Teleporting();
-        PlayerMove(4f);
+        //Teleporting();
+        //PlayerMove(4f);
     }
     
 
     public void PlayerMove(float speed) // 플레이어 이동
     {
+        this.speed = speed; //미안해..
         float x = Input.GetAxisRaw("Horizontal");
-        Vector2 moveDir = new Vector2(x, 0);
+        Vector2 moveDir = new Vector2(x, 0f);
         moveDir = moveDir.normalized;
         rigid.velocity = moveDir * speed;
     }
 
 
-    private void Teleporting() // 미리보기 화면으로 이동
-    {
-        if (priviewWindow.isContactingPreview == true && Input.GetKeyDown(KeyCode.E))
-        {
-            SceneManager.LoadScene("PreviewScenes"); // 미리보기 화면 씬으로 전환
-        }
-    }
+    //private void Teleporting() // 미리보기 화면으로 이동
+    //{
+    //    if (priviewWindow.isContactingPreview == true && Input.GetKeyDown(KeyCode.E))
+    //    {
+    //        SceneManager.LoadScene("PreviewScenes"); // 미리보기 화면 씬으로 전환
+    //    }
+    //}
 
 
     public void Buffering() // 버퍼링 기능
@@ -49,6 +50,7 @@ public class PlayerMovement : MonoBehaviour
         int i = Random.Range(0, 100);
         if (i < 50 && !isCoolTime) // 50퍼 확률로
         {
+            print("나 실행");
             PlayerMove(0.1f); // 버퍼링
         }
         else
