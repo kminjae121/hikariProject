d21f6d8f0fd40aad4db96d6b02aa161647bb1dc0
Yoonji21 <dayen.com.nate@gmail.com>
2024-06-26 01:00:46 +0900

    Firewall probability function

--- Assets/01_MemberFile/KYJ/01. Scripts/Firewall/FirewallWindow.cs
+++ Assets/01_MemberFile/KYJ/01. Scripts/Firewall/FirewallWindow.cs
@@ -7,9 +7,8 @@ public class FirewallWindow : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text; // 와이파이 연결 여부 텍스트
 
-    void Start()
+    void Awake()
     {
-        
     }
 
     void Update()
@@ -17,17 +16,17 @@ public class FirewallWindow : MonoBehaviour
         
     }
 
-    public void FirewallConnection()
+    public void FirewallConnection() // 방화벽 연결 시 텍스트 변경
     {
         if (FirewallManger.instance.FirewallOnOff == true) // 연결이 돼있을 때
         {
-            WiFiManager.instance.WifiOnOff = false; // 연결 끊기
+            FirewallManger.instance.FirewallOnOff = false;
             text.text = "연결 안 됨";
         }
 
         else
         {
-            FirewallManger.instance.FirewallOnOff = true; // 연결하기
+            FirewallManger.instance.FirewallOnOff = true;
             text.text = "연결됨";
         }
     }
--- Assets/01_MemberFile/KYJ/01. Scripts/Internet Features/InternetWindow.cs
+++ Assets/01_MemberFile/KYJ/01. Scripts/Internet Features/InternetWindow.cs
@@ -5,7 +5,17 @@ using UnityEngine.UI;
 
 public class InternetWindow : MonoBehaviour
 {
-    
+
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OnClickBack() // 뒤로가기 버튼 누를 시
+    {
+        gameObject.SetActive(false);
+    }
+
     public void OnClickWifi() // 와이파이 버튼 누를 시
     {
         if (WiFiManager.instance.WifiOnOff == false) // 와이파이 끊겼을 때
