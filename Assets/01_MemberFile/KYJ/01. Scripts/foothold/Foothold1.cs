e7a72c4e65b0a0bb1f2db0f0c39cba4700782be9
Yoonji21 <dayen.com.nate@gmail.com>
2024-07-09 19:27:38 +0900

    feat : Foothold2

--- Assets/01_MemberFile/KYJ/01. Scripts/foothold/Foothold1.cs
+++ Assets/01_MemberFile/KYJ/01. Scripts/foothold/Foothold1.cs
@@ -1,34 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Foothold1 : MonoBehaviour
 {
     private Rigidbody2D rigid;
     [SerializeField] private bool isTime;
 
+    float i = 3f;
+    Vector3 moveDir = new Vector3(0, 10f, 0);
+
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        
+    }
+
     private void OnCollisionStay2D(Collision2D other)
     {
-        isTime = true;
-        if (other.gameObject.CompareTag("Player") && isTime == true)
+        if (other.gameObject.CompareTag("Player") && isTime == false)
         {
             StartCoroutine(FootholdCool());
         }
+        StartCoroutine(ReturnFoothold());
+            transform.DOMoveY(20, i);
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        isTime = false;
+
     }
 
     IEnumerator FootholdCool()
     {
         yield return new WaitForSeconds(0.5f);
         rigid.bodyType = RigidbodyType2D.Dynamic;
+    }
+    
+    IEnumerator ReturnFoothold()
+    {
+        yield return new WaitForSeconds(3f);
     }
 }
