5d6790f302f20113811b26ca46bdcc70444df3b7
Yoonji21 <dayen.com.nate@gmail.com>
2024-07-16 08:58:46 +0900

    fix : Foothold

--- Assets/01_MemberFile/KYJ/01. Scripts/foothold/Foothold1.cs
+++ Assets/01_MemberFile/KYJ/01. Scripts/foothold/Foothold1.cs
@@ -12,27 +12,23 @@ public class Foothold1 : Foothold
         startPos = transform.position.y;
     }
 
-    private void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        //base.MoveFoothold(5f, 3f);
-    }   
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(DownMoveFoothold(0.5f));
+            StartCoroutine(UpMoveFoothold(4f));
 
-    //private void OnCollisionEnter2D(Collision2D other)
-    //{
-    //    if (other.gameObject.CompareTag("Player") && isBack == false)
-    //    {
-    //        StartCoroutine(FootholdCool(3f));
-    //        isBack = true;
-    //    }
-    //}
+        }
+    }
 
-    //protected override IEnumerator ReturnFoothold(float coolTime, float dgTime)
-    //{
-    //    return base.ReturnFoothold(coolTime, dgTime);
-    //}
+    protected override IEnumerator UpMoveFoothold(float stopTime)
+    {
+        return base.UpMoveFoothold(stopTime);
+    }
 
-    //protected override IEnumerator FootholdCool(float coolTime)
-    //{
-    //    return base.FootholdCool(coolTime);
-    //}
+    protected override IEnumerator DownMoveFoothold(float stopTime)
+    {
+        return base.DownMoveFoothold(stopTime);
+    }
 }
