6cb2cc5f5d2d22c7dee431299fc5bb694bd243b3
fishmaster <a01022639410@gmail.com>
2024-07-20 23:50:51 +0900

    feat : Capture

---
+++ Assets/01_MemberFile/KYH/Scripts/CaptureManager - Copy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureManager : MonoBehaviour
+{
+    [Header("Ä¸ÃÄ ¼³Á¤°ª")]
+    [SerializeField]
+    private Vector2 captureSize;
+    [SerializeField]
+    private LayerMask whatIsCaptureObj;
+
+    private void Update()
+    {
+        Capture();
+    }
+
+    public void Capture()
+    {
+        Collider2D[] captureObject = Physics2D.OverlapBoxAll(Input.mousePosition,captureSize,0,whatIsCaptureObj);
+
+        if(captureObject != null)
+        {
+            for(int i=0; i<captureObject.Length; i++)
+            {
+                captureObject[i]?.GetComponent<CaptureObject>().CaptureFinish();
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(gameObject.transform.position, captureSize);
+    }
+}
