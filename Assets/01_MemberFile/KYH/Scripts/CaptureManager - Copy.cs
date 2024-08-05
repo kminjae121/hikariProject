fd42b18afa1b2a06bd628fcdf6d32186d2a7b329
fishmaster <a01022639410@gmail.com>
2024-08-06 00:10:35 +0900

    Commit Please

--- Assets/01_MemberFile/KYH/Scripts/CaptureManager - Copy.cs
+++ Assets/01_MemberFile/KYH/Scripts/CaptureManager - Copy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,25 +11,40 @@ public class CaptureManager : MonoBehaviour
     [SerializeField]
     private LayerMask whatIsCaptureObj;
 
+    private Vector2 mousePos;
+    private bool isNowCapture;
+
     private void Update()
     {
         Capture();
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if(!isNowCapture)
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, mousePos, 10f * Time.deltaTime);
     }
 
     public void Capture()
     {
         Collider2D[] captureObject = Physics2D.OverlapBoxAll(gameObject.transform.position,captureSize,0,whatIsCaptureObj);
 
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && !isNowCapture)
         {
             if(captureObject != null)
             {
+                isNowCapture = true;
                 for(int i=0; i<captureObject.Length; i++)
                 {
-                    captureObject[i]?.GetComponent<CaptureObject>().CaptureFinish();
+                    captureObject[i].GetComponent<CaptureObject>().CaptureFinish();
                 }
+                StartCoroutine(WaitCaptureRoutine());
             }
+            print("캡쳐할 물ㅊㅔ가 없스니다");    
         }
+    }
+
+    private IEnumerator WaitCaptureRoutine()
+    {
+        yield return new WaitForSeconds(2f);
     }
 
     private void OnDrawGizmos()
