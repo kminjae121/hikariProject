fe2272dc2eba95e26dd9d92da22a1eef06bad71b
rlaals0203 <scientistmin@gmail.com>
2024-08-08 23:22:54 +0900

    feat : Angler Fish states

--- Assets/01_MemberFile/KimMin/01_Script/Fish/AnglerFish/AnglerFish.cs
+++ Assets/01_MemberFile/KimMin/01_Script/Fish/AnglerFish/AnglerFish.cs
@@ -2,7 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnglerFish : MonoBehaviour
+public class AnglerFish : FishEnemy
 {
+    public StateMachine _stateMachine;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _stateMachine.AddState(FishStateEnum.Move, new AnglerFishState(this, _stateMachine, "Move"));
+        _stateMachine.AddState(FishStateEnum.Chase, new AnglerFishState(this, _stateMachine, "Chase"));
+        _stateMachine.AddState(FishStateEnum.Attack, new AnglerFishState(this, _stateMachine, "Attack"));
+
+        _stateMachine.ChangeState(FishStateEnum.Move);
+    }
+
+    private void Update()
+    {
+        _stateMachine.CurrentState.UpdateState();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, detectRadius);
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, detectRadius + 2);
+        Gizmos.color = Color.white;
+    }
 }
--- Assets/01_MemberFile/KimMin/01_Script/Fish/AnglerFish/AnglerFishMoveState.cs
+++ Assets/01_MemberFile/KimMin/01_Script/Fish/AnglerFish/AnglerFishMoveState.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnglerFishState : FishState
+public class AnglerFishMoveState : AnglerFishState
 {
-    protected Fish _anglerFish;
-
-    public AnglerFishState(Fish _onwer, StateMachine state, string animHashName) : base(_onwer, state, animHashName)
+    public AnglerFishMoveState(Fish _onwer, StateMachine state, string animHashName) : base(_onwer, state, animHashName)
     {
-        _anglerFish = _onwer;
     }
 
     public override void Enter()
@@ -19,6 +16,11 @@ public class AnglerFishState : FishState
     public override void UpdateState()
     {
         base.UpdateState();
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            _stateMachine.ChangeState(FishStateEnum.Chase);
+        }
     }
 
     public override void Exit()
