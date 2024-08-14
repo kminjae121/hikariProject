b4471125f381a481c92daca7d2b7024ceea2ca1b
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2024-08-14 21:29:31 +0900

    feat : ElectricPan

--- Assets/01_MemberFile/KMJ/Object/ObjectGather.cs
+++ Assets/01_MemberFile/KMJ/Object/ObjectGather.cs
@@ -23,7 +23,8 @@ public class ObjectGarher : MonoBehaviour
     private Rigidbody2D _playerRigidBody;
     [SerializeField] private Vector2 _boxSize;
     [SerializeField] private int _jumpPower;
-    [SerializeField] private LayerMask _player;
+    [SerializeField] private int _flyingSpeed;
+    [SerializeField] private LayerMask _playerLayer;
 
 
     private void Start()
@@ -50,14 +51,24 @@ public class ObjectGarher : MonoBehaviour
         }
     }
 
-    private void ElectricFan()
+    private void ElectricFan(float multiplier = 1f)
     {
-        
+        Collider2D hit = Physics2D.OverlapBox(transform.position, _boxSize, 0, _playerLayer);
+
+        if(hit == true)
+        {
+            _playerRigidBody.gravityScale = 0;
+            _playerRigidBody.AddForce(Vector2.right * _flyingSpeed * multiplier, ForceMode2D.Impulse);
+        }
+        else
+        {
+            _playerRigidBody.gravityScale = 1;
+        }
     }
 
     private void Sofa(float multiplier = 1f)
     {
-        Collider2D hit = Physics2D.OverlapBox(transform.position, _boxSize, 0,_player);
+        Collider2D hit = Physics2D.OverlapBox(transform.position, _boxSize, 0,_playerLayer);
 
         if (hit == true)
         {
