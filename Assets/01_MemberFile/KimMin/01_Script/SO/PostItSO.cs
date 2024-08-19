596b75dfb9a36d7a878614a66551b597ea2974dc
rlaals0203 <scientistmin@gmail.com>
2024-08-20 01:11:07 +0900

    feat : post it so

---
+++ Assets/01_MemberFile/KimMin/01_Script/SO/PostItSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[CreateAssetMenu(menuName = "SO/PostIt")]
+public class PostItSO : ScriptableObject
+{
+    public Sprite sprite;
+    public TextMeshProUGUI text;
+}
