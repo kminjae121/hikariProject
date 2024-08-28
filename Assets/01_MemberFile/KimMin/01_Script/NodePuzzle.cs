a2646b2c380c027cc9b95ac9b0d19aeaa1b60d88
rlaals0203 <scientistmin@gmail.com>
2024-08-29 00:42:16 +0900

    perf : NodePuzzle

--- Assets/01_MemberFile/KimMin/01_Script/NodePuzzle.cs
+++ Assets/01_MemberFile/KimMin/01_Script/NodePuzzle.cs
@@ -9,50 +9,68 @@ using UnityEngine.EventSystems;
 
 public class NodePuzzle : MonoBehaviour
 {
-    [SerializeField] private Button[] _reqireButtons;
-    [SerializeField] private Button _desireButton;
+    private Button[] _valueBtn;
+    private TextMeshProUGUI[] _valueTxt;
+    private RawImage[] _nodes;
+    private int[] _valueOrder = new int[4];
+    private int[,] _valueArr = new int[4, 10];
 
-    [SerializeField] private TextMeshProUGUI[] _reqireTexts;
-    [SerializeField] private TextMeshProUGUI _desireText;
-    [SerializeField] private TextMeshProUGUI _sumText;
+    private TextMeshProUGUI _reqTxt;
+    private TextMeshProUGUI _sumTxt;
 
+    private int _sum = 0;
+
+    [Range(0, 100000), SerializeField] private int _range;
     [SerializeField] private int[] Answer = new int[4];
-
-    [Range(0, 100000)]
-    [SerializeField] private int _range;
-
-    [Header("디버그 전용")]
     [SerializeField] private int _answer;
-    [SerializeField] private int[,] _reqireNumbers = new int[4, 10];
-    [SerializeField] private int[] _reqireCount = new int[4];
 
     private void Awake()
     {
+        Assignment();
         InitializePuzzle();
     }
 
-    private void InitializePuzzle()
+    private void Assignment()
     {
-        _answer = Random.Range(0, _range);
-        StartCoroutine(DesireTextAnim());
+        _valueBtn = new Button[4];
+        _valueTxt = new TextMeshProUGUI[4];
+        _nodes = new RawImage[4];
 
-        int[] answerNumbers = new int[4];
-        int sum = 0;
+        _reqTxt = GameObject.Find("DesireNumberText").GetComponent<TextMeshProUGUI>();
+        _sumTxt = GameObject.Find("SumText").GetComponent<TextMeshProUGUI>();
 
-        for (int i = 0; i < 3; i++)
-        {
-            answerNumbers[i] = Random.Range
-                (i * (_answer / 4), _answer);
-
-            sum += answerNumbers[i];
-        }
-
-        answerNumbers[3] = _answer - sum;
 
         for (int i = 0; i < 4; i++)
         {
-            Answer[i] = answerNumbers[i];
+            _valueBtn[i]
+                = GameObject.Find($"ValueButton{i + 1}").GetComponent<Button>();
+            _valueTxt[i]
+                = GameObject.Find($"ValueText{i + 1}").GetComponent<TextMeshProUGUI>();
+            _nodes[i]
+                = GameObject.Find($"Node{i + 1}").GetComponent<RawImage>();
         }
+    }
+
+    private void InitializePuzzle() //퍼즐 초기화
+    {
+        int sum = 0;
+        _answer = Random.Range(0, _range);
+        StartCoroutine(DesireTextAnim());
+
+        int[] answer = new int[4];
+
+        for (int i = 0; i < 3; i++)
+        {
+            answer[i] = Random.Range
+                (i * (_answer / 2), _answer);
+
+            sum += answer[i];
+        }
+
+        answer[3] = _answer - sum;
+
+        for (int i = 0; i < 4; i++)
+            Answer[i] = answer[i];
 
         for(int i = 0; i < 4; i++)
         {
@@ -60,9 +78,9 @@ public class NodePuzzle : MonoBehaviour
             for (int j = 0; j < 10; j++)
             {
                 if (j == index)
-                    _reqireNumbers[i, j] = answerNumbers[i];
+                    _valueArr[i, j] = answer[i];
                 else
-                    _reqireNumbers[i, j] = Random.Range(j^3 * 100, j^3 * 1000);
+                    _valueArr[i, j] = Random.Range(j^3 * 100, j^3 * 1000);
             }
         }
 
@@ -70,47 +88,54 @@ public class NodePuzzle : MonoBehaviour
         {
             SetReqireButton(i);
         }
+
+        GetSum();
     }
 
-    private void SetReqireButton(int reqIndex) //버튼 현재 카운트 설정
+    private void SetReqireButton(int value) //버튼 현재 카운트 설정
     {
-        int rand = Random.Range(0, 10);
+        int rand = Random.Range(1, 11);
 
-        _reqireCount[reqIndex] = rand;
-        _reqireTexts[reqIndex].text = _reqireCount[reqIndex].ToString();
+        _valueOrder[value] = rand;
+        _valueTxt[value].text = _valueArr[value, rand - 1].ToString();
     }
 
     public void OnReqireClick() //버튼 클릭
     {
-        int sum = 0;
-        string button =
-            EventSystem.current.currentSelectedGameObject.name.ToString();
+        string button = EventSystem.current
+            .currentSelectedGameObject.name.ToString();
 
         int index = int.Parse(button.Substring(button.Length - 1)) - 1;
 
-        _reqireTexts[index].text = _reqireNumbers
-            [index, _reqireCount[index]].ToString();
+        GetSum();
 
-        _reqireCount[index]++;
+        _valueTxt[index].text = _valueArr
+            [index, _valueOrder[index]].ToString();
 
-        if (_reqireCount[index] >= 9)
-            _reqireCount[index] = 0;
+        _valueOrder[index]++;
+
+        if (_valueOrder[index] >= 10)
+            _valueOrder[index] = 0;
+    }
+
+    private void GetSum()
+    {
+        _sum = 0;
 
         for (int i = 0; i < 4; i++)
-        {
-            sum += _reqireNumbers[i, _reqireCount[i]];
-        }
-        _sumText.text = sum.ToString();
+            _sum += _valueArr[i, _valueOrder[i]];
+
+        _sumTxt.text = $"합계 : {_sum}";
     }
 
     private IEnumerator DesireTextAnim() //숫자 바뀌는 효과
     {
         for (int i = 0; i < 25; i++)
         {
-            _desireText.text = Random.Range(0, _range).ToString();
+            _reqTxt.text = Random.Range(0, _range).ToString();
             yield return new WaitForSeconds(0.05f);
         }
 
-        _desireText.text = _answer.ToString();
+        _reqTxt.text = _answer.ToString();
     }
 }
