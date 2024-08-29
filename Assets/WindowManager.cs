9544845ac6fd2e8efa03cb5b39c33357c8ee6607
fishmaster <a01022639410@gmail.com>
2024-08-30 00:13:56 +0900

    hf

--- Assets/WindowManager.cs
+++ Assets/WindowManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Video;
 
 public class WindowManager : MonoBehaviour
 {
@@ -21,12 +22,16 @@ public class WindowManager : MonoBehaviour
         yield return new WaitForSeconds(1f);
         noise.m_AmplitudeGain = 0.6f;
         noise.m_FrequencyGain = 0.1f;
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(2f);
         noise.m_AmplitudeGain = 3f;
         noise.m_FrequencyGain = 4f;
         yield return new WaitForSeconds(1f);
         noise.m_AmplitudeGain = 0f;
         noise.m_FrequencyGain = 0f;
+
+        VideoPlayer player = null;
+        double videoTime = player.length;
+        double videoCurrentTime = player.time;
 
     }
 }
