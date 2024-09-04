e77c896d1332d0431094f3cf8bf310c9166c3419
Yoonji21 <dayen.com.nate@gmail.com>
2024-09-04 20:15:20 +0900

    fix : Luminescent Plants

--- Assets/01_MemberFile/KYJ/01. Scripts/Object/BrightPlants.cs
+++ Assets/01_MemberFile/KYJ/01. Scripts/Object/BrightPlants.cs
@@ -9,21 +9,23 @@ public class BrightPlants : MonoBehaviour
     [SerializeField]
     private LuminescentPlants luminescentPlants;
     [SerializeField]
-    private Light2D light;
+    private BrightFoothold brightFoothold;
     [SerializeField]
-    public BrightFoothold brightFoothold;
+    private Light2D _light;
+    [SerializeField]
 
-    public float brightStep ;
+    public float brightStep;
 
     public Vector2 pos;
     private float size = 3f;
     public LayerMask foothold;
 
-    public bool isReach;
+    public bool _isReach;
+    public bool canPlant;
 
     private void Awake()
     {
-        light.intensity = 0;
+        _light.intensity = 0;
 
         luminescentPlants.OnPlants += BrightnessRange;
         luminescentPlants.OnPlants += BrightnessControl;
@@ -31,37 +33,39 @@ public class BrightPlants : MonoBehaviour
 
     private void BrightnessControl()
     {
-
         if (Input.GetKeyDown(KeyCode.I))
         {
-            light.intensity += 2;
-            light.intensity = Mathf.Clamp(light.intensity, 0, 8);
-            brightStep = light.intensity / 2;
+            _light.intensity += 2;
+            _light.intensity = Mathf.Clamp(_light.intensity, 0, 8);
+            brightStep = _light.intensity / 2;
             print(brightStep);
         }
         else if (Input.GetKeyDown(KeyCode.U))
         {
-            light.intensity -= 2;
-            light.intensity = Mathf.Clamp(light.intensity, 0, 8);
-            brightStep = light.intensity / 2;
+            _light.intensity -= 2;
+            _light.intensity = Mathf.Clamp(_light.intensity, 0, 8);
+            brightStep = _light.intensity / 2;
             print(brightStep);
         }
     }
 
     private void BrightnessRange()
     {
-        Collider2D collision = Physics2D.OverlapCircle(transform.position, size, foothold);
-        if (collision)
+        Collider2D[] collision = Physics2D.OverlapCircleAll(transform.position, size, foothold);
+
+        for (int i = 0; i < collision.Length; i++)
         {
-            isReach = true;
-            brightFoothold.BrightnessDetection();
-        }
-        else if(!collision)
-        {
-            isReach = false;
-            brightFoothold.BrightnessDetection();
+            if (collision != null)
+            {
+                collision[i].GetComponent<BrightFoothold>().BrightnessDetection(true, brightStep);
+            }
+            if (!collision[i])
+            {
+                brightFoothold.BrightnessDetection(false, brightStep);
+            }
         }
     }
+
 
     void OnDrawGizmos() // 범위 그리기
     {
