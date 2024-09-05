c3a37c66614f3abe4228bb5eba2a6d5f3768f7b8
fishmaster <a01022639410@gmail.com>
2024-09-06 00:58:25 +0900

    feat : WinAssets

--- Assets/01_MemberFile/KimMin/01_Script/UI/Intro.cs
+++ Assets/01_MemberFile/KimMin/01_Script/UI/Intro.cs
@@ -9,6 +9,12 @@ public class Intro : MonoBehaviour
     [SerializeField] private RawImage _glowScreen;
     [SerializeField] private RawImage _background;
 
+    [SerializeField]
+    private ParticleSystem particleSystem;
+
+    [SerializeField]
+    private Image blueBackGround;
+
     private Sequence glowScreenSeq;
 
 
@@ -16,7 +22,6 @@ public class Intro : MonoBehaviour
     {
         _background.gameObject.SetActive(true);
         _glowScreen.gameObject.SetActive(true);
-
 
         glowScreenSeq = DOTween.Sequence()
             .OnStart(() =>
@@ -28,8 +33,11 @@ public class Intro : MonoBehaviour
             .Append(_glowScreen.transform.DOScaleX(200, 7f)).SetEase(Ease.OutCubic)
             .Join(_glowScreen.transform.DOScaleY(0.4f, 7f)).SetEase(Ease.OutCubic)
             .Append(_glowScreen.transform.DOScaleY(120, 2.5f)).SetEase(Ease.OutCubic)
+            .JoinCallback(() => particleSystem.Play())
+            .Join(blueBackGround.DOColor(new Color(150, 170, 255, 255), 5f))
             .Append(_background.DOFade(0, 0f))
             .Append(_glowScreen.DOFade(0, 1.25f))
+            .JoinCallback(()=> particleSystem.Stop())
             .OnComplete(() => transform.gameObject.SetActive(false));
     }
 }
