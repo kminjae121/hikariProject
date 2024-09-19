b87605f6436249fcbcd6f497674ae130cda50b71
fishmaster <a01022639410@gmail.com>
2024-09-19 19:31:45 +0900

    feat : LockFade

--- Assets/01_MemberFile/KYH/Scripts/LockfadeIn.cs
+++ Assets/01_MemberFile/KYH/Scripts/LockfadeIn.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public class LockfadeIn : MonoBehaviour
 {
-    public Vector2 size;
+    public SpriteRenderer lockObj;
+
+    Sequence _seq;
+
+    // const int LIST_COUNT = 3;
+    // List<int> _list = new List<int>(LIST_COUNT);
 
     public void LockFade()
     {
-        StartCoroutine(FadeRoutine());
+        //Span<int> arr = new int[2];
+
+        //arr[0] = 0;
+        //arr[1] = 0;
+
+        //transform.TransformPoints(new Vector3[] { });
+
+        SafeKill(_seq);
+
+        _seq = DOTween.Sequence()
+            .Append(lockObj.DOFade(duration: 1, endValue: 1))
+            .Append(lockObj.DOFade(duration: 1, endValue: 0));
     }
 
-    private IEnumerator FadeRoutine()
+    public void SafeKill(Tween tween)
     {
-        gameObject.GetComponent<SpriteRenderer>().DOFade(1, 1).WaitForCompletion();
-        yield return new WaitForSeconds(1f);
-        gameObject.GetComponent<SpriteRenderer>().DOFade(0, 1).WaitForCompletion();
+        if(tween is not null && tween.IsActive())
+            tween.Complete();
     }
+
 }
