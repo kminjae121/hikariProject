62975810d9d20e47abf21b4d57ae52c7895fd530
dlckdgh0414 <ci5976547@gmail.com>
2024-09-22 00:52:07 +0900

    fix : Scroll

--- Assets/01_MemberFile/KYH/Test/BackgroundScroll.cs
+++ Assets/01_MemberFile/KYH/Test/BackgroundScroll.cs
@@ -1,21 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour
 {
-	[SerializeField] private float _moveSpeed;
-    [SerializeField] private float scrollRange;
-
-   public Transform target;
+    [SerializeField] private float _moveSpeed;
+    private SpriteRenderer _backsprite;
+    private Vector2 backGroundScrollOffset = Vector2.zero;
+    private void Awake()
+    {
+        _backsprite = GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
-        transform.position += Vector3.right * _moveSpeed * Time.deltaTime;
+        BackgroundScrolling();
+    }
 
-        if (transform.position.x <= -scrollRange)
-        {
-            transform.position = target.position + Vector3.right * scrollRange;
-        }
+    private void BackgroundScrolling()
+    {
+        backGroundScrollOffset.x += (_moveSpeed * Time.deltaTime);
+        _backsprite.material.mainTextureOffset = backGroundScrollOffset;
     }
 }
 
