b0d74f5ce6db61a81cd8f866513cf1f319e7ebd9
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2024-09-27 01:10:22 +0900

    fix - WalkAnimation

--- Assets/01_MemberFile/KMJ/Script/SoundManager.cs
+++ Assets/01_MemberFile/KMJ/Script/SoundManager.cs
@@ -11,18 +11,55 @@ public class SoundManager : MonoBehaviour
     [SerializeField] AudioSource music3;
     [SerializeField] AudioSource music4;
 
+    private string _currentScene;
+
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
-
         music1 = GameObject.Find("CaptureSoundAsset").GetComponent<AudioSource>();
         music2 = GameObject.Find("MainSoundAsset").GetComponent<AudioSource>();
         music3 = GameObject.Find("SeaSoundAsset").GetComponent<AudioSource>();
         music4 = GameObject.Find("VirusSoundAsset").GetComponent<AudioSource>();
+
+    }
+
+    private void Start()
+    {
+        music1.Pause();
+        music2.Pause();
+        music3.Pause();
+        music4.Pause();
+        _currentScene = SceneManager.GetActiveScene().name;
+
+        switch (_currentScene)
+        {
+            case "WindowScene":
+                music2.Play();
+                break;
+            case "CaptureStage1":
+                music1.Play();
+                break;
+            case "CaptureStage2":
+                music1.Play();
+                break;
+            case "CaptureStage3":
+                music1.Play();
+                break;
+            case "CaptureStage4":
+                music1.Play();
+                break;
+            case "CaptureStage5":
+                music1.Play();
+                break;
+            case "SeaScene":
+                music3.Play();
+                break;
+            default:
+                music4.Play();
+                break;
+        }
     }
 
     private void Update()
     {
-        
     }
 }
