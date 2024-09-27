b0426c0ec35ceabe675256abc92045553de580ac
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2024-09-27 12:09:55 +0900

    feat - StageSound

--- Assets/01_MemberFile/KMJ/Script/CaptureStartText.cs
+++ Assets/01_MemberFile/KMJ/Script/CaptureStartText.cs
@@ -2,20 +2,21 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class CaptureStartText : MonoBehaviour
 {
     [SerializeField] private TMP_Text _text;
     [SerializeField] private GameObject _textMember;
+    private Color[] color;
 
     private void Awake()
     {
-       
     }
 
     private void Start()
     {
-        _text.TextUpDownMove(3f, 2.5f,TextStyle.Moving | TextStyle.UI | TextStyle.FadeIn);
+        _text.TextUpDownMove(7f, Color.black, 2.4f,TextStyle.Moving | TextStyle.UI | TextStyle.FadeIn);
     }
 
     private void Update()
@@ -24,6 +25,12 @@ public class CaptureStartText : MonoBehaviour
             _textMember.SetActive(false);
     }
 
+    
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(8);
+        _textMember.SetActive(false);
+    }
 
     
 
