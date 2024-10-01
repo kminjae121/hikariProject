34d7fa8213898acada0c1b3051a0f4f3a8690e84
Yoonji21 <dayen.com.nate@gmail.com>
2024-10-01 20:16:57 +0900

    fix : Backgroound

---
+++ Assets/01_MemberFile/KYJ/01. Scripts/UI/Quest/TutorialObj.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TutorialObj : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obj;
+    private int[] objNumber;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void ObjectRelease()
+    {
+        //obj[].GetComponent<Renderer>().material.DOColor();
+    }
+}
