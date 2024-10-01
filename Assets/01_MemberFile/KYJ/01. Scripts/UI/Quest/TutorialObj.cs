b38e745a37e8eabc651439969b77f79238579ec6
Yoonji21 <dayen.com.nate@gmail.com>
2024-10-01 20:31:03 +0900

    feat : ObjObjectRelease

--- Assets/01_MemberFile/KYJ/01. Scripts/UI/Quest/TutorialObj.cs
+++ Assets/01_MemberFile/KYJ/01. Scripts/UI/Quest/TutorialObj.cs
@@ -6,20 +6,27 @@ using DG.Tweening;
 public class TutorialObj : MonoBehaviour
 {
     [SerializeField] private GameObject[] obj;
-    private int[] objNumber;
+    private int objNumber;
 
     private void Awake()
     {
-        
+        for (int i = -0; i < obj.Length; i++)
+        {
+            obj[i].GetComponent<SpriteRenderer>().color = Color.black;
+        }
     }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ObjectRelease();
+        }
     }
 
     private void ObjectRelease()
     {
-        //obj[].GetComponent<Renderer>().material.DOColor();
+        obj[objNumber].GetComponent<SpriteRenderer>().DOColor(Color.white, 1);
+        objNumber++;
     }
 }
