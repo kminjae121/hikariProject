beffba28be613f1dae3c8f0b3986859cf3488d1a
Vuzuruu <qqoaqw123456@gmail.com>
2024-10-07 19:36:56 +0900

    feat - StageFix

---
+++ Assets/00_Scenes/KimDY/Script/Curser.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class Curser : MonoBehaviour
+{
+   
+
+    Vector3 velocity =  Vector2.zero;
+    private void Awake()
+    {
+     
+    }
+
+  
+    }
+
