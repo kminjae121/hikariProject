935ab637ce424315af8799f1b950f2467521f2da
fishmaster <a01022639410@gmail.com>
2024-10-10 00:27:40 +0900

    fix : Sea Chat

--- Assets/01_MemberFile/KimMin/01_Script/Dialogue/PlayerDialogue.cs
+++ Assets/01_MemberFile/KimMin/01_Script/Dialogue/PlayerDialogue.cs
@@ -8,9 +8,9 @@ public class PlayerDialogue : MonoBehaviour
     {
         PlayerChatBoxManager.Instance
             .Show("으으 어지러워", 3f, true)
-            .Show("이곳은 어디일까?", 3f, true)
-            .Show("깊은 심해인거 같아", 3f, true)
-            .Show("이곳을 빠져나가야겠어!", 3.5f, true)
+            .Show("여긴 어디지?", 3f, true)
+            .Show("깊은 심해로 들어왔나봐..", 3f, true)
+            .Show("빨리 자료나 찾고 밖으로 나가자", 3.5f, true)
             .End();
 
         QuestPopupUI.Instance.QuestTxt();
@@ -19,10 +19,10 @@ public class PlayerDialogue : MonoBehaviour
     public void LuminescentPlantDialogue()
     {
         PlayerChatBoxManager.Instance
-            .Show("이 밝은 물체는 뭘까?", 3f, true)
-            .Show("과연 히카리.. 밝은것을 좋아하네", 4f, true)
-            .Show("이 물체를 들고 I와 U를 눌러서 밝기를 조절할 수 있어", 4.5f, true)
-            .Show("그리고 밝기를 이용해 특정한 물체들을 동작시킬 수 있어", 4f, true)
+            .Show("이 꽃은 뭐지?", 3f, true)
+            .Show("안에서 빛이 세어나오고 있어", 4f, true)
+            .Show("신기하네..주워가자", 4.5f, true)
+            .Show("( I 와 U 로 밝기를 조절할 수 있습니다 )", 4f, true)
             .End();
 
        QuestPopupUI.Instance.QuestTxt();
@@ -31,18 +31,18 @@ public class PlayerDialogue : MonoBehaviour
     public void LeafWallDialogue()
     {
         PlayerChatBoxManager.Instance
-            .Show("이것은 잎사귀 기둥이야", 3f, true)
-            .Show("이 기둥은 특정한 밝기에만 잠깐 사라지지", 4.5f, true)
-            .Show("특정한 밝기를 잘 찾아서 기둥을 통과 해보자!", 5f, true)
+            .Show("이 잎사귀 전에 인터넷에서 본적있어", 3f, true)
+            .Show("특정 밝기를 싫어한다고 했는데", 4.5f, true)
+            .Show("이 식물을 이용해볼까?", 5f, true)
             .End();
     }
 
     public void AnglerFishDialogue()
     {
         PlayerChatBoxManager.Instance
-            .Show("이건 아귀 조심 표지판이야!", 3f, true)
-            .Show("아귀는 정말 무시무시한 존재지", 3.5f, true)
-            .Show("하지만 걱정마 만약 너가 불빛을 내지 않는다면 아귀가 공격하는 일은 없을테니까!", 6.5f, true)
+            .Show("잠깐 여기 어떻게 생물체가 있지?", 3f, true)
+            .Show("바이러스인가봐", 3.5f, true)
+            .Show("아귀가 가까히 오면 불을 끄고 자취를 숨기자", 6.5f, true)
             .End();
 
         QuestPopupUI.Instance.QuestTxt();
@@ -51,20 +51,20 @@ public class PlayerDialogue : MonoBehaviour
     public void BrightFlowerDialogue()
     {
         PlayerChatBoxManager.Instance
-            .Show("이곳은 다양한 꽃들을 밟아서 위로 올라가야해", 4f, true)
-            .Show("꽃들은 특정한 밝기에만 꽃봉우리가 열릴거야", 4f, true)
-            .Show("왼쪽 밑에 있는 \"책 모양 버튼\"을 클릭해 사전을 열어봐", 5f, true)
-            .Show("사전에는 무슨 꽃이 무슨 밝기에 열리는지 적혀있어!", 5f, true)
-            .Show("사전을 잘 참조해봐!", 5f, true)
+            .Show("이 꽃 드디어 찾았네", 4f, true)
+            .Show("이 꽃도 특정 밝기를 좋아한다고 했지", 4f, true)
+            .Show("( 왼쪽 밑에 있는 \"사전\"을 클릭해 정보를 확인할 수 있습니다 )", 5f, true)
+            .Show("이정도면 과제에 넣을건 다 찾은것 같은데?", 5f, true)
+            .Show("이제 출구만 찾으면 되겠어", 5f, true)
             .End();
     }
 
     public void OctopusDialogue()
     {
         PlayerChatBoxManager.Instance
-            .Show("이 박스 안에는 문어가 있어", 3f, true)
-            .Show("문어는 특정 밝기에만 반응해", 3f, true)
-            .Show("문어가 반응하면 박스의 윗부분을 밀어내 더 높이 올라갈 수 있을거야", 5.5f, true)
+            .Show("안에 문어가 들어있네?", 3f, true)
+            .Show("특정 불빛을 싫어한다고 했던것 같아", 3f, true)
+            .Show("이 꽃을 이용하면 문어가 내가 올라가는 걸 도와줄거야", 5.5f, true)
             .End();
     }
 }
